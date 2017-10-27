0be03566d53647b9fb1afd9d6a9497c6126eda10
Brian Domanski <domanski.brian@gmail.com>
2017-10-27 13:39:32 -0400

    Tried to restructure code, isn't quite working yet

--- app/src/main/java/com/example/brian/checkin/LaunchScreen.java
+++ app/src/main/java/com/example/brian/checkin/LaunchScreen.java
@@ -1,58 +1,51 @@
 package com.example.brian.checkin;
 
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.common.AccountPicker;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.FusedLocationProviderApi;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.ActivityRecognition;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.places.PlaceLikelihood;
-import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.Places;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import android.content.pm.PackageManager;
+import android.accounts.AccountManager;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.location.Location;
-import android.os.Handler;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.content.ContextCompat;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.Manifest;
-
-import java.util.ArrayList;
 
 public class LaunchScreen extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
-    // The geographical location where the device is currently located. That is, the last-known
-    // location retrieved by the Fused Location Provider.
-    private Location mLastKnownLocation;
+    // Code that should be received when requesting account
+    private static final int REQUEST_CODE_EMAIL = 1;
 
-    private FirebaseDatabase database = FirebaseDatabase.getInstance();
-    private final DatabaseReference userRef = database.getReference("userInput/");
-    private final DatabaseReference placesRef = database.getReference("placesAPI/");
+    private EditText text_box;
 
-    // Used for selecting the current place.
     private GoogleApiClient mGoogleApiClient;
-    private LocationRequest locationRequest;
+
+    // Reference to Firebase Database
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference userRef;
+    private DatabaseReference pushRef;
+
+    // Calls places API
+    private LocationService places;
+
+    // User ID to push to Firebase
+    private String userID = "null";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_screen);
+        text_box = findViewById(R.id.location_input);
 
         // Construct a GeoDataClient.
         mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -64,119 +57,68 @@ public class LaunchScreen extends FragmentActivity implements GoogleApiClient.Co
                 .addOnConnectionFailedListener(this)
                 .build();
 
-        locationRequest = new LocationRequest();
-        locationRequest.setInterval(10_000);
-        locationRequest.setFastestInterval(5_000);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        places = new LocationService();
+        places.setContext(this);
+
+        mGoogleApiClient.connect();
+
+        //requestAccount();
     }
 
     public void onQueryClick(View v) {
-        EditText text_box = findViewById(R.id.location_input);
+        userRef = database.getReference(userID);
+        pushRef = userRef.push();
+        pushRef.child("userInput").setValue(text_box.getText().toString());
 
-        userRef.push().setValue(text_box.getText().toString());
-
-        getCurrentPlaces();
+        places.placesRef = pushRef;
+        places.requestLocationUpdates();
+        places.getCurrentPlaces();
     }
 
-    public void getCurrentPlaces() {
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            System.out.println("Permission Granted\n");
-            mLastKnownLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-            if(mLastKnownLocation != null) System.out.println(mLastKnownLocation.toString());
-        } else {
-            System.out.println("Permission Not Granted\n");
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+    public void requestAccount() {
+
+        Intent intent = AccountPicker.newChooseAccountIntent(null, null,
+                new String[]{GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE},
+                false, null, null, null, null);
+
+        try {
+            startActivityForResult(intent, REQUEST_CODE_EMAIL);
+        } catch (ActivityNotFoundException e) {
+            userRef = database.getReference(userID);
         }
-
-        PendingResult<PlaceLikelihoodBuffer> placeResult = Places.PlaceDetectionApi
-                .getCurrentPlace(mGoogleApiClient, null);
-
-        placeResult.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
-            @Override
-            public void onResult(PlaceLikelihoodBuffer likelyPlaces) {
-
-                // Debug info printed to console
-                Status status = likelyPlaces.getStatus();
-                System.out.println(status.isSuccess());
-                System.out.println(status.getStatusCode());
-                System.out.println(status.getStatusMessage());
-                System.out.println(status.getStatus());
-
-                TextView output = findViewById(R.id.output);
-                String name;
-                float likelihood;
-
-                // Print depending on number of places
-                output.setText(likelyPlaces.getCount() > 0 ? "Nearby Places:\n" : "No Nearby Places\n");
-
-                // Print out
-                for (PlaceLikelihood placeLikelihood : likelyPlaces) {
-                    name = placeLikelihood.getPlace().getName().toString();
-                    likelihood = placeLikelihood.getLikelihood();
-
-                    if(likelihood > 0) {
-                        output.append(name + ": " + likelihood);
-                        placesRef.child("Name").push().setValue(name);
-                        placesRef.child("Likelihood").push().setValue(likelihood);
-                    }
-
-                    // Debug info
-                    System.out.println(placeLikelihood.getPlace().getName().toString());
-                    System.out.println(placeLikelihood.getLikelihood());
-                }
-                likelyPlaces.release();
-            }
-        });
     }
 
     @Override
-    public void onRequestPermissionsResult(final int requestCode, @NonNull final String[] permissions, @NonNull final int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 123) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission granted.
-                getCurrentPlaces();
-            } else {
-                // User refused to grant permission. You can add AlertDialog here
-                Toast.makeText(this, "You didn't give permission to access device location", Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-
-    private void requestLocationUpdates() {
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            System.out.println("Permission Granted\n");
-            mLastKnownLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-            if(mLastKnownLocation != null) System.out.println(mLastKnownLocation.toString());
-        } else {
-            System.out.println("Permission Not Granted\n");
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_EMAIL && resultCode == RESULT_OK) {
+            userID = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+            return;
         }
 
-        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,  locationRequest, this);
+        // Set location to push data
+        userRef = database.getReference(userID);
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
-    public void onLocationChanged(Location location) {
-        mLastKnownLocation = location;
-    }
+    public void onLocationChanged(Location location) {}
 
     @Override
     protected void onStart() {
         super.onStart();
-        mGoogleApiClient.connect();
-        database.goOnline();
     }
 
     @Override
     protected void onStop() {
         mGoogleApiClient.disconnect();
-        database.goOffline();
         super.onStop();
     }
 
     @Override
-    public void onConnectionFailed(ConnectionResult result) {}
+    public void onConnectionFailed(ConnectionResult result) {
+        System.err.println(result.toString());
+    }
 
     @Override
     public void onConnectionSuspended(int i) {}
@@ -184,6 +126,9 @@ public class LaunchScreen extends FragmentActivity implements GoogleApiClient.Co
 
     @Override
     public void onConnected(Bundle bundle) {
-        requestLocationUpdates();
+        System.out.println("Time to request location updates");
+        places.mGoogleApiClient = mGoogleApiClient;
+        places.requestLocationUpdates();
+        System.out.println("Done");
     }
 }
---
+++ app/src/main/java/com/example/brian/checkin/LocationService.java
@@ -0,0 +1,162 @@
+package com.example.brian.checkin;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.ActivityRecognition;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
+import com.google.android.gms.location.places.Places;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by Brian on 10/27/2017.
+ *
+ */
+
+public class LocationService extends FragmentActivity implements LocationListener {
+
+    // The geographical location where the device is currently located. That is, the last-known
+    // location retrieved by the Fused Location Provider.
+    private Location mLastKnownLocation;
+
+    private TextView output;
+    public GoogleApiClient mGoogleApiClient;
+
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference pushRef;
+    public DatabaseReference placesRef;
+
+    // Used for selecting the current place.
+    private LocationRequest locationRequest;
+
+    // Used to access main activity's context
+    private Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        output = findViewById(R.id.output);
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(10_000);
+        locationRequest.setFastestInterval(5_000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    public void getCurrentPlaces() {
+        if(ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            System.out.println("Permission Granted\n");
+            mLastKnownLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if(mLastKnownLocation != null) System.out.println(mLastKnownLocation.toString());
+        } else {
+            System.out.println("Permission Not Granted\n");
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+        }
+
+        PendingResult<PlaceLikelihoodBuffer> placeResult = Places.PlaceDetectionApi
+                .getCurrentPlace(mGoogleApiClient, null);
+
+        placeResult.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
+            @Override
+            public void onResult(PlaceLikelihoodBuffer likelyPlaces) {
+
+                // Debug info printed to console
+                Status status = likelyPlaces.getStatus();
+                System.out.println(status.isSuccess());
+                System.out.println(status.getStatusCode());
+                System.out.println(status.getStatusMessage());
+                System.out.println(status.getStatus());
+
+                String name;
+                float likelihood;
+
+                // Print depending on number of places
+                output.setText(likelyPlaces.getCount() > 0 ? "Nearby Places:\n" : "No Nearby Places\n");
+
+                // Print out
+                for (PlaceLikelihood placeLikelihood : likelyPlaces) {
+                    name = placeLikelihood.getPlace().getName().toString();
+                    likelihood = placeLikelihood.getLikelihood();
+
+                    if(likelihood > 0) {
+                        output.append(name + ": " + likelihood + '\n');
+
+                        pushRef = placesRef.child("placesAPI").push();
+                        pushRef.child("Name").setValue(name);
+                        pushRef.child("Likelihood").setValue(likelihood);
+                    }
+
+                    // Debug info
+                    System.out.println(placeLikelihood.getPlace().getName().toString());
+                    System.out.println(placeLikelihood.getLikelihood());
+                }
+                likelyPlaces.release();
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(final int requestCode, @NonNull final String[] permissions, @NonNull final int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 123) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted.
+                getCurrentPlaces();
+            } else {
+                // User refused to grant permission. You can add AlertDialog here
+                Toast.makeText(this, "You didn't give permission to access device location", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void requestLocationUpdates() {
+        if(ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            System.out.println("Permission Granted\n");
+            mLastKnownLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if(mLastKnownLocation != null) System.out.println(mLastKnownLocation.toString());
+        } else {
+            System.out.println("Permission Not Granted\n");
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+        }
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,  locationRequest, this);
+    }
+
+    public void setContext(Context in) {
+        context = in;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastKnownLocation = location;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+}
